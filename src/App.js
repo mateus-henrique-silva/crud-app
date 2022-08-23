312baa040a6141133671d9f8d80867a0fbff460b
mateus-henrique-silva <henrique.s@gsuite.com.br>
2022-08-23 16:59:13 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sign from "./Pages/Sign/Sign";
+import Admin from "./Pages/Admin/Admin";
+const Private = ({ Item }) => {
+  const { signed } = true;
 
+  return signed > 0 ? <Item /> : <Sign />;
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+        <Route exact path="/home" element={<Private Item={Admin} />} />
+          <Route exact element={<Sign />} path="/" />
+          <Route element={<Sign />} path="*" />
+        </Routes>
+      </Router>
     </div>
   );
 }
